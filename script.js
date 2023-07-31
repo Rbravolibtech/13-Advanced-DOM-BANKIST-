307007d204a734cf41372407d6121a6cb10bae96
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-07-31 12:22:09 -0600

    191 js propagation practice

--- script.js
+++ script.js
@@ -142,19 +142,47 @@ btnScrollTo.addEventListener('click', function (e) {
   //   behavior: 'smooth',
   // });
 
-  section1.scrollIntoView({ behavior: 'smooth' });
-});
+  //   section1.scrollIntoView({ behavior: 'smooth' });
+  // });
 
-const h1 = document.querySelector('h1');
-
-h1.addEventListener('mouseenter', function (e) {
-  alert('addEventListener: Great! You are reading the heading ');
-
-  h1.removeEventListener('mouseenter', alertH1);
-
-  setTimeout(() => h1.addEventListener('mouseenter', alertH1), 3000);
+  // const h1 = document.querySelector('h1');
 
   // h1.addEventListener('mouseenter', function (e) {
   //   alert('addEventListener: Great! You are reading the heading ');
+
+  //   h1.removeEventListener('mouseenter', alertH1);
+
+  //   setTimeout(() => h1.addEventListener('mouseenter', alertH1), 3000);
+
+  //   // h1.addEventListener('mouseenter', function (e) {
+  //   //   alert('addEventListener: Great! You are reading the heading ');
+  //   // });
   // });
+
+  ////////////////// EVENT PROPAGATION IN PRACTICE //////////////////////////
+
+  //rgb(255,, 255 ,255)
+  const randomInt = (min, max) =>
+    Math.floor(Math.random() * (max - min + 1) + min);
+  const randomColor = () =>
+    `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+  document.querySelector('.nav__link').addEventListener('click', function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log('LINK', e.target, e.currentTarget);
+    console.log(e.currentTarget === this);
+
+    // Stop propagation
+    // e.stopPropagation();
+  });
+
+  document.querySelector('.nav__links').addEventListener('click', function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log('CONTAINER', e.target, e.currentTarget);
+  });
+
+  document.querySelector('.nav').addEventListener('click', function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log('NAV', e.target, e.currentTarget);
+  });
 });

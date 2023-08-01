20b96fc8bfec19f398ff7feac296eae8930a2b64
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-08-01 11:42:23 -0600

    196 sticky navigation js

--- script.js
+++ script.js
@@ -136,6 +136,17 @@ btnScrollTo.addEventListener('click', function (e) {
   nav.addEventListener('mouseover', handleHover.bind(0.5));
 
   nav.addEventListener('mouseout', handleHover.bind(1));
+
+  ////////////// STICKY NAVIGATION /////////////////
+  const initialCoords = section1.getBoundingClientRect();
+
+  window.addEventListener('scroll', function () {
+    console.log(window.scrollY);
+
+    if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+    else nav.classList.remove('sticky');
+  });
+
   ////////////// SELECTING CREATING AND DELETING ELEMENTS /////////////////
 
   // console.log(document.documentElement);
